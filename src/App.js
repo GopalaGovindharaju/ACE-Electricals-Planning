a6e5a4ca6f10eb158d1460ef3c8770e0173d4daa
gopala <gopivelraj7169@gmail.com>
2023-05-20 11:43:29 +0530

    Started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>ACE Electricals</h1>
-        <p>sample</p>
-    </div>
+    
   );
 }
 
