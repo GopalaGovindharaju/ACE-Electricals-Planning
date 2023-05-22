2c2681380f66d5cd59d0516f1358f97c8419a6db
gopala <gopivelraj7169@gmail.com>
2023-05-22 09:09:50 +0530

    empty

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Banner from './componenet/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import popcomplete from './componenet/Order.js'
 import Order from './componenet/Order'
 function App() {
   return (
-    <div className="App">
-      <Banner/>
-      <Order/>
-    </div>
+      <div className="app">
+        <Banner/>
+        <Order/>
+
+      </div>
   );
 }
 
