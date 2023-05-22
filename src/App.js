117aa97f6714271342550a5292623e2091b2e79e
Arthiclaks <chitraraveendran.com>
2023-05-22 08:30:52 +0530

    files are added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Gridlay from './componenet/BOMDetails';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <br></br>
+      <br></br>
 
-    </div>
+      <center>
+        <h1 className="custom-heading neon-effect">ACE ELECTRICALS</h1>
+      </center>
+        <br></br>
+        
+        <div class="page-container">
+          <Gridlay />
+        </div>
+
+    </div>   
   );
 }
 
