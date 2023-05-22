952cda8a85ed3b32e213386f8812c33074236a0f
gopala <gopivelraj7169@gmail.com>
2023-05-22 16:16:10 +0530

    banner and order details

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import './App.css';
-//import Banner from './componenet/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Order from './componenet/Order'
-import Gridlay from './componenet/BOMDetails';
-import React, { useState } from 'react';
-import Popup from './componenet/popup';
-
-//import {ReactDOM} from 'react';
-
+import Banner from './componenet/Banner';
+import Order from './componenet/Order';
 function App() {
-  const [showPopup, setShowPopup] = useState(true);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setShowPopup(false);
-  };
 
   return (
-     <div><center>
-      <h1 className="m-4 custom-heading neon-effect">ACE ELECTRICALS</h1></center>
-      {showPopup && (
-        <Popup
-          onSubmit={handleSubmit}
-        />
-      )}
-
-      {!showPopup && (
-        <div>
-          <div>
-      <div class="page-container">
-          <Gridlay />
-      </div>
-    </div>
-        </div>
-      )}
-    </div>
+    <div className='App'>
+     <Banner/>
+     <Order/>
+     </div>
   );
 }
 
---
+++ src/componenet/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Banner() {
+
+  return (
+    <div>
+      <div class="p-2 mb-2 text-white bg-gradient text-center fs-4 fw-bold" style={{ backgroundColor: '#ED7D31' }}>ACE Electricals Planning</div>
+    </div>
+  )
+}
+
+export default Banner
